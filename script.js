dcde68e9fb3446eafbe14997b4a29250751181c8
Upender Arukonda <109522168+UArukonda@users.noreply.github.com>
2026-01-27 18:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const student = {
+	name = "John"
+}
+
+function getKeys(obj){
+	let keys = []
+	for(let key in obj){
+		keys.push(key)
+	}
+	return keys
+}
+
+console.log(getKeys(student))
